f9ed20657850008fde6d7a5e6ea7ff310ed9f26b
Sumedh38 <85394026+Sumedh38@users.noreply.github.com>
2021-09-14 00:57:42 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+var hour = document.getElementById("hour");
+var minute = document.getElementById("minute");
+var second = document.getElementById("second");
+var digitalclock = document.getElementById("digitalclock");
+
+function setClockTime() {
+  const day = new Date();
+  const hh = day.getHours();
+  const mm = day.getMinutes();
+  const ss = day.getSeconds();
+
+  const hourDeg = hh * 30 + mm * 0.5;
+  const minuteDeg = mm * 6 + ss * 0.1;
+  const secondDeg = ss * 6;
+
+  hour.style.transform = `rotateZ(${hourDeg}deg)`;
+  minute.style.transform = `rotateZ(${minuteDeg}deg)`;
+  second.style.transform = `rotateZ(${secondDeg}deg)`;
+
+  digitalclock.innerText = `${hh % 12 < 10 ? "0" : ""}${hh % 12}:${
+    mm < 10 ? "0" : ""
+  }${mm}${hh > 12 ? "PM" : "AM"}`;
+}
+setClockTime();
+
+setInterval(setClockTime, 1000);
